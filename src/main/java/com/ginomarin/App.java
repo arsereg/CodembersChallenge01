3ff900f334ba434233fab6ee2840e2887c7b780a
arsereg <arsereg@gmail.com>
2023-11-04 05:18:30 -0600

    Finished solution

--- src/main/java/com/ginomarin/App.java
+++ src/main/java/com/ginomarin/App.java
@@ -1,13 +1,23 @@
 package com.ginomarin;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        WordsService wordsService = new WordsService();
+        System.out.println("-------------------------------------------");
+        System.out.println(wordsService.getWords(loadWords()));
+        System.out.println("-------------------------------------------");
+    }
+
+    public static String loadWords() throws IOException {
+        return new String(Files.readAllBytes(Paths.get("src/main/java/com/ginomarin/words.txt")));
     }
 }
